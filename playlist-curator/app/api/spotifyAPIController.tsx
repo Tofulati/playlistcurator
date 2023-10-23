8daee7196dc7a8374233028db895649a7c620a4c
addychen2 <addisonxchen@gmail.com>
2023-10-22 21:11:59 -0700

    spotify api like 90% finished

--- playlist-curator/app/api/spotifyAPIController.tsx
+++ playlist-curator/app/api/spotifyAPIController.tsx
@@ -4,17 +4,22 @@ import React, { useEffect, useState } from "react";
 const CLIENT_ID = '707bf634189c4e8594a161f7ef9808a4'
 const CLIENT_SECRET = '56568d00ab634c5fa7f2d8a361b22de4'
 
-export default function SpotifyAPIController() {
-    const [searchInput, setSearchInput] = useState('')
-    const [accessToken, setAccessToken] = useState('')
-    const [artist, setArtist] = useState('')
+export class SpotifyAPIController {
 
-    const playlistTrackNames = [];
-    const playlistTrackCovers = [];
-    const playlistTrackArtists = [];
+    constructor(){
+        this.searchInput = ''
+        this.accessToken = '';
+        this.artist = '';
+        this.gotToken = false;
 
-    useEffect(() => {
-    // Gets access token so you can make REST API requests
+
+        this.playlistTrackNames = [];
+        this.playlistTrackCovers = [];
+        this.playlistTrackArtists = [];
+
+    }
+
+    async getAccessToken(){
         var authParams = {
             method: 'POST',
             headers: {
@@ -22,126 +27,126 @@ export default function SpotifyAPIController() {
             },
             body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
         }
-        fetch('https://accounts.spotify.com/api/token',authParams)
+        var res = await fetch('https://accounts.spotify.com/api/token',authParams)
             .then(res => res.json())
-            .then(data => setAccessToken(data.access_token));
+            .then(data => {this.accessToken = data.access_token})
+        console.log("acces token func " + this.accessToken)
+    }
 
-    }, [])
-
-    const getSong = (trackEndPoint) => {
-        const result = fetch(`https://api.spotify.com/v1/tracks/${trackEndPoint}`, {
+    async getSong(trackEndPoint) {
+        await this.getAccessToken();
+        console.log("song func " + this.accessToken)
+        var result = await fetch(`https://api.spotify.com/v1/tracks/${trackEndPoint}`, {
             method: 'GET',
-            headers: { 'Authorization' : 'Bearer ' + accessToken}
+            headers: { 'Authorization' : 'Bearer ' + this.accessToken}
         })
             .then(res => res.json())
             .then(data => console.log(data));
     }
 
-    const getSongArtist = (trackEndPoint) => {
-        const result = fetch(`https://api.spotify.com/v1/tracks/${trackEndPoint}`, {
+    async getSongArtist(trackEndPoint) {
+        await this.getAccessToken();
+        const result = await fetch(`https://api.spotify.com/v1/tracks/${trackEndPoint}`, {
             method: 'GET',
-            headers: { 'Authorization' : 'Bearer ' + accessToken}
+            headers: { 'Authorization' : 'Bearer ' + this.accessToken}
         })
             .then(res => res.json())
-            .then(data => setArtist(data.artists[0].id))
+            .then(data => console.log(data.artists[0].id))
     }
 
-    const getArtist = (artistEndPoint) => {
-        const result = fetch(`https://api.spotify.com/v1/artists/${artistEndPoint}`, {
+    async getArtist(artistEndPoint){
+        await this.getAccessToken();
+        const result = await fetch(`https://api.spotify.com/v1/artists/${artistEndPoint}`, {
             method: 'GET',
-            headers: { 'Authorization' : 'Bearer ' + accessToken}
+            headers: { 'Authorization' : 'Bearer ' + this.accessToken}
         })
             .then(res => res.json())
             .then(data => console.log(data))
     }
 
-    const getArtistGenre = (artistEndPoint) => {
-        const result = fetch(`https://api.spotify.com/v1/artists/${artistEndPoint}`, {
+    async getArtistGenre(artistEndPoint){
+        await this.getAccessToken();
+        const result = await fetch(`https://api.spotify.com/v1/artists/${artistEndPoint}`, {
             method: 'GET',
-            headers: { 'Authorization' : 'Bearer ' + accessToken}
+            headers: { 'Authorization' : 'Bearer ' + this.accessToken}
         })
             .then(res => res.json())
             .then(data => console.log(data.genres))
     }
 
-    const getTrackCover = (trackEndPoint) => {
-        const result = fetch(`https://api.spotify.com/v1/tracks/${trackEndPoint}`, {
+    async getTrackCover(trackEndPoint){
+        await this.getAccessToken();
+        const result = await fetch(`https://api.spotify.com/v1/tracks/${trackEndPoint}`, {
             method: 'GET',
-            headers: { 'Authorization' : 'Bearer ' + accessToken}
+            headers: { 'Authorization' : 'Bearer ' + this.accessToken}
         })
             .then(res => res.json())
             .then(data => console.log(data.album.images[0].url))
     }
 
-    const getPlaylist = (playlistEndPoint) => {
-        const result = fetch(`https://api.spotify.com/v1/playlists/${playlistEndPoint}`, {
+    async getPlaylist(playlistEndPoint){
+        await this.getAccessToken();
+        const result = await fetch(`https://api.spotify.com/v1/playlists/${playlistEndPoint}`, {
             method: 'GET',
-            headers: { 'Authorization' : 'Bearer ' + accessToken}
+            headers: { 'Authorization' : 'Bearer ' + this.accessToken}
         })
             .then(res => res.json())
             .then(data => console.log(data))
     }
 
-    const getPlaylistTracks = (playlistEndPoint) => {
-        const result = fetch(`https://api.spotify.com/v1/playlists/${playlistEndPoint}`, {
+    async getPlaylistTracks(playlistEndPoint){
+        await this.getAccessToken();
+        const result = await fetch(`https://api.spotify.com/v1/playlists/${playlistEndPoint}`, {
             method: 'GET',
-            headers: { 'Authorization' : 'Bearer ' + accessToken}
+            headers: { 'Authorization' : 'Bearer ' + this.accessToken}
         })
             .then(res => res.json())
             .then(data => console.log(data.tracks.items))
     }
 
-    const getPlaylistTrackNames = (playlistEndPoint) => {
-        const result = fetch(`https://api.spotify.com/v1/playlists/${playlistEndPoint}`, {
+    async getPlaylistTrackNames(playlistEndPoint){
+        await this.getAccessToken();
+        const result = await fetch(`https://api.spotify.com/v1/playlists/${playlistEndPoint}`, {
             method: 'GET',
-            headers: { 'Authorization' : 'Bearer ' + accessToken}
+            headers: { 'Authorization' : 'Bearer ' + this.accessToken}
         })
             .then(res => res.json())
             .then(data => {
                 for(let i = 0; i < data.tracks.limit; i++){
-                    playlistTrackNames[i] = data.tracks.items[i].track.name;
+                    this.playlistTrackNames[i] = data.tracks.items[i].track.name;
                     //console.log(data.tracks.items[i].track.name)
                 }
              })
     }
 
-    const getPlaylistTrackArtists = (playlistEndPoint) => {
-        const result = fetch(`https://api.spotify.com/v1/playlists/${playlistEndPoint}`, {
+    async getPlaylistTrackArtists(playlistEndPoint){
+        await this.getAccessToken();
+        const result = await fetch(`https://api.spotify.com/v1/playlists/${playlistEndPoint}`, {
             method: 'GET',
-            headers: { 'Authorization' : 'Bearer ' + accessToken}
+            headers: { 'Authorization' : 'Bearer ' + this.accessToken}
         })
             .then(res => res.json())
             .then(data => {
                 for(let i = 0; i < data.tracks.limit; i++){
-                    playlistTrackArtists[i] = data.tracks.items[i].track.artists[0].name;
+                    this.playlistTrackArtists[i] = data.tracks.items[i].track.artists[0].name;
                     //console.log(data.tracks.items[i].track.name)
                 }
              })
     }
 
-    const getPlaylistTrackCovers = (playlistEndPoint) => {
-        const result = fetch(`https://api.spotify.com/v1/playlists/${playlistEndPoint}`, {
+    async getPlaylistTrackCovers(playlistEndPoint){
+        await this.getAccessToken();
+        const result = await fetch(`https://api.spotify.com/v1/playlists/${playlistEndPoint}`, {
             method: 'GET',
-            headers: { 'Authorization' : 'Bearer ' + accessToken}
+            headers: { 'Authorization' : 'Bearer ' + this.accessToken}
         })
             .then(res => res.json())
             .then(data => {
                 for(let i = 0; i < data.tracks.limit; i++){
-                    playlistTrackCovers[i] = data.tracks.items[i].track.album.images[0].url;
+                    this.playlistTrackCovers[i] = data.tracks.items[i].track.album.images[0].url;
                     //console.log(data.tracks.items[i].album.images[0].url)
                 }
              })
     }
-
-
-
-    return(
-        //getSongArtist('5sdQOyqq2IDhvmx2lHOpwd'),
-        //getArtistGenre(artist)
-        //getTrackCover('5sdQOyqq2IDhvmx2lHOpwd')
-        getPlaylistTrackArtists('7eypSrxMD0YCIfwRKYtV4C'),
-        console.log(playlistTrackArtists)
-
-    )
 
 }
